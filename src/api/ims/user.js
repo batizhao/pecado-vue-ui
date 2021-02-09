d44fb85efd67d06ad36d9188354823c8b3df1eb9
batizhao <zhaobati@gmail.com>
2021-02-09 19:36:55 +0800

    用户管理

--- src/api/ims/user.js
+++ src/api/ims/user.js
@@ -1,127 +1,49 @@
 import request from '@/utils/request'
-import { praseStrEmpty } from "@/utils/ruoyi";
 
 // 查询用户列表
 export function listUser(query) {
   return request({
-    url: '/system/user/list',
-    method: 'get',
+    url: '/ims/users',
     params: query
   })
 }
 
 // 查询用户详细
-export function getUser(userId) {
-  return request({
-    url: '/system/user/' + praseStrEmpty(userId),
-    method: 'get'
-  })
+export function getUser(id) {
+  return request(`/ims/user/${id}`);
 }
 
-// 添加用户
-export function addUser(data) {
+// 添加或者编辑用户
+export function addOrUpdateUser(data) {
   return request({
-    url: '/system/user',
+    url: '/ims/user',
     method: 'post',
     data: data
   })
 }
 
-// 编辑用户
-export function updateUser(data) {
-  return request({
-    url: '/system/user',
-    method: 'put',
-    data: data
-  })
+// 删除用户
+export function deleteUser(ids) {
+  return request.delete(`/ims/user?ids=${ids}`);
 }
 
-// 删除用户
-export function delUser(userId) {
+// 角色状态编辑
+export function changeUserStatus(id, status) {
+  const data = {
+    id,
+    status
+  }
   return request({
-    url: '/system/user/' + userId,
-    method: 'delete'
+    url: '/ims/user/status',
+    method: 'post',
+    data: data
   })
 }
 
 // 导出用户
 export function exportUser(query) {
   return request({
-    url: '/system/user/export',
-    method: 'get',
+    url: '/ims/user/export',
     params: query
-  })
-}
-
-// 用户密码重置
-export function resetUserPwd(userId, password) {
-  const data = {
-    userId,
-    password
-  }
-  return request({
-    url: '/system/user/resetPwd',
-    method: 'put',
-    data: data
-  })
-}
-
-// 用户状态编辑
-export function changeUserStatus(userId, status) {
-  const data = {
-    userId,
-    status
-  }
-  return request({
-    url: '/system/user/changeStatus',
-    method: 'put',
-    data: data
-  })
-}
-
-// 查询用户个人信息
-export function getUserProfile() {
-  return request({
-    url: '/system/user/profile',
-    method: 'get'
-  })
-}
-
-// 编辑用户个人信息
-export function updateUserProfile(data) {
-  return request({
-    url: '/system/user/profile',
-    method: 'put',
-    data: data
-  })
-}
-
-// 用户密码重置
-export function updateUserPwd(oldPassword, newPassword) {
-  const data = {
-    oldPassword,
-    newPassword
-  }
-  return request({
-    url: '/system/user/profile/updatePwd',
-    method: 'put',
-    params: data
-  })
-}
-
-// 用户头像上传
-export function uploadAvatar(data) {
-  return request({
-    url: '/system/user/profile/avatar',
-    method: 'post',
-    data: data
-  })
-}
-
-// 下载用户导入模板
-export function importTemplate() {
-  return request({
-    url: '/system/user/importTemplate',
-    method: 'get'
   })
 }
