5e942a4c0dc6927950501a71d57ff34eff597a69
Bati Zhao <zhaobati@gmail.com>
2021-06-10 15:34:35 +0800

    同步 formarker 1.3 元数据 datasource，增加 oa 模块。

---
+++ src/api/oa/comment.js
@@ -0,0 +1,49 @@
+import request from '@/utils/request'
+
+// 分页查询审批
+export function listComments(query) {
+  return request({
+    url: '/oa/comments',
+    params: query
+  })
+}
+
+// 查询审批
+export function listComment(query) {
+  return request({
+    url: '/oa/comment',
+    params: query
+  })
+}
+
+// 查询审批详细
+export function getComment(id) {
+  return request(`/oa/comment/${id}`);
+}
+
+// 添加或者编辑审批
+export function addOrUpdateComment(data) {
+  return request({
+    url: '/oa/comment',
+    method: 'post',
+    data: data
+  })
+}
+
+// 删除审批
+export function deleteComment(ids) {
+  return request.delete(`/oa/comment?ids=${ids}`);
+}
+
+// 审批状态编辑
+export function changeCommentStatus(id, status) {
+  const data = {
+    id,
+    status
+  }
+  return request({
+    url: '/oa/comment/status',
+    method: 'post',
+    data: data
+  })
+}
---
+++ src/views/oa/comment/index.vue
@@ -0,0 +1,374 @@
+<template>
+  <div class="app-container">
+    <el-form :model="queryParams" ref="queryForm" :inline="true" v-show="showSearch">
+      <el-form-item>
+        <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
+        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+      </el-form-item>
+    </el-form>
+    <el-row :gutter="10" class="mb8">
+      <el-col :span="1.5">
+        <el-button
+          type="primary"
+          plain
+          icon="el-icon-plus"
+          size="mini"
+          @click="handleAdd"
+          v-hasPermi="['oa:comment:add']"
+        >添加</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="success"
+          plain
+          icon="el-icon-edit"
+          size="mini"
+          :disabled="single"
+          @click="handleUpdate"
+          v-hasPermi="['oa:comment:edit']"
+        >编辑</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="danger"
+          plain
+          icon="el-icon-delete"
+          size="mini"
+          :disabled="multiple"
+          @click="handleDelete"
+          v-hasPermi="['oa:comment:delete']"
+        >删除</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="warning"
+          plain
+          icon="el-icon-download"
+          size="mini"
+          @click="handleExport"
+          v-hasPermi="['oa:comment:export']"
+        >导出</el-button>
+      </el-col>
+      <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
+    </el-row>
+    <el-table v-loading="loading" :data="commentList" @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55" align="center" />
+      <el-table-column label="id" align="center" prop="id" />
+      <el-table-column label="标题" align="center" prop="title" />
+      <el-table-column label="意见" align="center" prop="comment" />
+      <el-table-column label="创建时间" align="center" prop="createTime" width="180">
+        <template slot-scope="scope">
+          <span>{{ parseTime(scope.row.createTime) }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+        <template slot-scope="scope">
+          <el-button
+            type="text"
+            icon="el-icon-edit"
+            @click="handleUpdate(scope.row)"
+            v-hasPermi="['oa:comment:edit']"
+          >编辑</el-button>
+          <el-button
+            type="text"
+            icon="el-icon-delete"
+            @click="handleDelete(scope.row)"
+            v-hasPermi="['oa:comment:delete']"
+          >删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    
+    <pagination
+      v-show="total>0"
+      :total="total"
+      :page.sync="queryParams.current"
+      :limit.sync="queryParams.size"
+      @pagination="getList"
+    />
+    <!-- 添加或编辑审批对话框 -->
+    <el-dialog :title="title" :visible.sync="open" width="500px" v-if='open' append-to-body>   
+      <fm-generate-form
+        :data="jsonData"
+        :remote-option="dynamicData"
+        :remote="remoteFuncs"
+        :value="form"
+        ref="form"
+      >
+      </fm-generate-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="submitForm">确 定</el-button>
+        <el-button @click="cancel">取 消</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import { listComments, listComment, getComment, deleteComment, addOrUpdateComment, changeCommentStatus } from "@/api/oa/comment";
+import { downLoadExcel } from "@/utils/download";
+
+
+export default {
+  name: "Comment",
+  components: {
+  },
+  data() {
+    return {
+      jsonData: {
+  "list" : [ {
+    "type" : "input",
+    "icon" : "icon-input",
+    "options" : {
+      "width" : "100%",
+      "defaultValue" : "",
+      "required" : false,
+      "requiredMessage" : "",
+      "dataType" : "",
+      "dataTypeCheck" : false,
+      "dataTypeMessage" : "",
+      "pattern" : "",
+      "patternCheck" : false,
+      "patternMessage" : "",
+      "placeholder" : "",
+      "customClass" : "",
+      "disabled" : false,
+      "labelWidth" : 100,
+      "hidden" : true,
+      "dataBind" : true,
+      "showPassword" : false,
+      "remoteFunc" : "",
+      "remoteOption" : ""
+    },
+    "name" : "",
+    "key" : "60c1b5e36454f3c2a8163f13",
+    "model" : "id",
+    "rules" : [ ]
+  }, {
+    "type" : "input",
+    "icon" : "icon-input",
+    "options" : {
+      "width" : "100%",
+      "defaultValue" : "",
+      "required" : true,
+      "requiredMessage" : "",
+      "dataType" : "",
+      "dataTypeCheck" : false,
+      "dataTypeMessage" : "",
+      "pattern" : "",
+      "patternCheck" : false,
+      "patternMessage" : "",
+      "placeholder" : "",
+      "customClass" : "",
+      "disabled" : false,
+      "labelWidth" : 100,
+      "hidden" : false,
+      "dataBind" : true,
+      "showPassword" : false,
+      "remoteFunc" : "",
+      "remoteOption" : ""
+    },
+    "name" : "标题",
+    "key" : "60c1b5e36454f3c2a8163f14",
+    "model" : "title",
+    "rules" : [ {
+      "required" : true,
+      "message" : "Required"
+    } ]
+  }, {
+    "type" : "input",
+    "icon" : "icon-input",
+    "options" : {
+      "width" : "100%",
+      "defaultValue" : "",
+      "required" : true,
+      "requiredMessage" : "",
+      "dataType" : "",
+      "dataTypeCheck" : false,
+      "dataTypeMessage" : "",
+      "pattern" : "",
+      "patternCheck" : false,
+      "patternMessage" : "",
+      "placeholder" : "",
+      "customClass" : "",
+      "disabled" : false,
+      "labelWidth" : 100,
+      "hidden" : false,
+      "dataBind" : true,
+      "showPassword" : false,
+      "remoteFunc" : "",
+      "remoteOption" : ""
+    },
+    "name" : "意见",
+    "key" : "60c1b5e36454f3c2a8163f15",
+    "model" : "comment",
+    "rules" : [ {
+      "required" : true,
+      "message" : "Required"
+    } ]
+  } ],
+  "config" : {
+    "labelWidth" : 100,
+    "labelPosition" : "right",
+    "size" : "small",
+    "customClass" : "",
+    "ui" : "element",
+    "layout" : "horizontal",
+    "labelCol" : 3,
+    "width" : "100%",
+    "hideLabel" : false,
+    "hideErrorMessage" : false,
+    "dataSource" : null
+  }
+},
+      dynamicData: {
+      },
+      remoteFuncs: {
+      },
+      // 遮罩层
+      loading: true,
+      // 选中数组
+      ids: [],
+      // 非单个禁用
+      single: true,
+      // 非多个禁用
+      multiple: true,
+      // 总条数
+      total: 0,
+      // 显示搜索条件
+      showSearch: true,
+      // 审批表格数据
+      commentList: [],
+      // 弹出层标题
+      title: "",
+      // 是否显示弹出层
+      open: false,
+      // 查询参数
+      queryParams: {
+        current: 1,
+        size: 10,
+      },
+      // 表单参数
+      form: {},
+      // 表单校验
+      rules: {
+        title: [
+          { required: true, message: "标题不能为空", trigger: "blur" }
+        ],
+        comment: [
+          { required: true, message: "意见不能为空", trigger: "blur" }
+        ],
+      }
+    };
+  },
+  created() {
+    this.getList();
+  },
+  methods: {
+    /** 查询审批列表 */
+    getList() {
+      this.loading = true;
+      listComments(this.queryParams).then(response => {
+        this.commentList = response.data.records;
+        this.total = response.data.total;
+        this.loading = false;
+      });
+    },
+    // 审批状态编辑
+    handleStatusChange(row) {
+      let text = row.status === "open" ? "启用" : "停用";
+      this.$confirm('确认要"' + text + '""' + row.name + '"吗?', "警告", {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning"
+        }).then(function() {
+          return changeCommentStatus(row.id, row.status);
+        }).then(() => {
+          this.msgSuccess(text + "成功");
+        }).catch(function() {
+          row.status = row.status === "open" ? "close" : "open";
+        });
+    },
+    // 取消按钮
+    cancel() {
+      this.open = false;
+      this.reset();
+    },
+    // 表单重置
+    reset() {
+      this.form = {};
+    },
+    /** 搜索按钮操作 */
+    handleQuery() {
+      this.queryParams.current = 1;
+      this.getList();
+    },
+    /** 重置按钮操作 */
+    resetQuery() {
+      this.resetForm("queryForm");
+      this.handleQuery();
+    },
+    // 多选框选中数据
+    handleSelectionChange(selection) {
+      this.ids = selection.map(item => item.id)
+      this.single = selection.length!==1
+      this.multiple = !selection.length
+    },    
+    /** 添加按钮操作 */
+    handleAdd() {
+      this.reset();
+      this.open = true;
+      this.title = "添加审批";
+    },
+    /** 编辑按钮操作 */
+    handleUpdate(row) {
+      this.reset();
+      const id = row.id || this.ids
+      getComment(id).then(response => {
+        this.form = response.data;
+        this.open = true;
+        this.title = "编辑审批";
+      });
+    },
+    /** 提交按钮 */
+    submitForm() {
+      this.$refs.form.getData().then(data => {
+        addOrUpdateComment(data).then(response => {
+          this.msgSuccess("保存成功");
+          this.open = false;
+          this.getList();
+        });
+      })
+    },
+    /** 删除按钮操作 */
+    handleDelete(row) {
+      const ids = row.id || this.ids;
+      this.$confirm('是否确认删除审批编号为"' + ids + '"的数据项?', "警告", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      }).then(function() {
+        return deleteComment(ids);
+      }).then(() => {
+        this.getList();
+        this.msgSuccess("删除成功");
+      }).catch(error => {
+        reject(error)
+      })
+    },
+    /** 导出按钮操作 */
+    handleExport() {
+      const queryParams = this.queryParams;
+      this.$confirm('是否确认导出所有审批数据项?', "警告", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      }).then(function() {
+        return exportComment(queryParams);
+      }).then(response => {
+        this.download(response.msg);
+      })
+    }
+  }
+};
+</script>
