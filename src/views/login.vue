83d696066d2b17d615e779914b030e235073466f
Bati Zhao <zhaobati@gmail.com>
2021-08-26 14:57:10 +0800

    增加验证码（Redis实现）

--- src/views/login.vue
+++ src/views/login.vue
@@ -18,7 +18,7 @@
           <svg-icon slot="prefix" icon-class="password" class="el-input__icon input-icon" />
         </el-input>
       </el-form-item>
-      <!-- <el-form-item prop="code">
+      <el-form-item prop="code">
         <el-input
           v-model="loginForm.code"
           auto-complete="off"
@@ -31,7 +31,7 @@
         <div class="login-code">
           <img :src="codeUrl" @click="getCode" class="login-code-img"/>
         </div>
-      </el-form-item> -->
+      </el-form-item>
       <el-checkbox v-model="loginForm.rememberMe" style="margin:0px 0px 25px 0px;">记住密码</el-checkbox>
       <el-form-item style="width:100%;">
         <el-button
@@ -54,7 +54,7 @@
 </template>
 
 <script>
-import { getCodeImg } from "@/api/login";
+import { getCaptcha } from "@/api/login";
 import Cookies from "js-cookie";
 import { encrypt, decrypt } from '@/utils/jsencrypt'
 
@@ -78,7 +78,9 @@ export default {
         password: [
           { required: true, trigger: "blur", message: "密码不能为空" }
         ],
-        // code: [{ required: true, trigger: "change", message: "验证码不能为空" }]
+        code: [
+          { required: true, trigger: "change", message: "验证码不能为空" }
+        ]
       },
       loading: false,
       redirect: undefined
@@ -93,16 +95,16 @@ export default {
     }
   },
   created() {
-    // this.getCode();
+    this.getCode();
     this.getCookie();
   },
   methods: {
-    // getCode() {
-    //   getCodeImg().then(res => {
-    //     this.codeUrl = "data:image/gif;base64," + res.img;
-    //     this.loginForm.uuid = res.uuid;
-    //   });
-    // },
+    getCode() {
+      getCaptcha().then(res => {
+        this.codeUrl = "data:image/gif;base64," + res.data.code;
+        this.loginForm.uuid = res.data.uuid;
+      });
+    },
     getCookie() {
       const username = Cookies.get("username");
       const password = Cookies.get("password");
@@ -130,7 +132,7 @@ export default {
             this.$router.push({ path: this.redirect || "/" }).catch(()=>{});
           }).catch(() => {
             this.loading = false;
-            // this.getCode();
+            this.getCode();
           });
         }
       });
